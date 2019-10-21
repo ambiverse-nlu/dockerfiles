3033b63d2fbbf55ee61e208494e27f515595b7d2
Dragan Milchevski (CR/PJ-AI-R6) <Dragan.Milchevski@de.bosch.com>
2019-10-21 10:27:41 +0200

    Adapting the image to use the latest cassandra image, and adding ready proby to check if cassandra is up and running. Removing the part where cassandra downloads custom dumps. This is now done through the NLU service.

--- nlu-db-cassandra/README.md
+++ nlu-db-cassandra/README.md
@@ -4,7 +4,7 @@
 
 # AmbiverseNLU Cassandra Database Dockerfile
 
-This Dockerfile is an extension of the [cassandra:3.11](https://github.com/docker-library/cassandra/blob/4474c6c5cc2a81ee57c5615aae00555fca7e26a6/3.11/Dockerfile)
+This Dockerfile is an extension of the [cassandra:3.11.4](https://github.com/docker-library/cassandra/blob/f98d3fc5282a99cdfe1ec8aa808d6313080137c0/3.11/Dockerfile)
 official docker image. It extends the `docker-endpoint.sh` script from the [this](https://github.com/emschimmel/cassandra/blob/master/3.11/docker-entrypoint.sh) pull-request 
 that allows initializing the image with `*.cql` dumps.  With this image you are ready to use the [AmbiverseNLU](https://github.com/ambiverse-nlu/ambiverse-nlu) service with Cassandra database.
 
@@ -33,7 +33,7 @@ docker run -d --restart=always --name nlu-db-cassandra \
  -p 7199:7199 \
  -p 9160:9160 \
  -e DATABASE_NAME=aida_20180120_cs_de_en_es_ru_zh_v18 \
- ambiverse/nlu-db-cassandra
+ ambiverse/nlu-db-cassandra:3.11.4
 ~~~~~~~~
 
 ## Connecting it from the AmbiverseNLU container
@@ -53,57 +53,74 @@ docker run -d --restart=always --name ambiverse-nlu \
 ### ... or via `docker-stack deploy` or `docker-compose`
 Example service-cassandra.yml for [AmbiverseNLU](https://github.com/ambiverse-nlu/ambiverse-nlu):
 ~~~~~~~~
-version: '3.1'
+version: '3.6'
 
 services:
-
-  db:
-    image: ambiverse/nlu-db-cassandra
+  cassandra:
+    image: ambiverse/nlu-cassandra-cassandra:3.11.4
     restart: always
+    deploy:
+      resources:
+        limits:
+          memory: 32G
+      restart_policy:
+        condition: on-failure
+        max_attempts: 3
+        window: 120s
     environment:
       DATABASE_NAME: aida_20180120_cs_de_en_es_ru_zh_v18
-
+      MAX_HEAP_SIZE: 16G
+      HEAP_NEWSIZE: 800M
+    volumes:
+      - "cassandra_data:/var/lib/cassandra"
+    networks:
+      nlunet:
+        aliases:
+          - nlu-cassandra
+    healthcheck:
+      test: /bin/bash -c /ready-probe.sh || exit 1
+      interval: 1m
+      timeout: 10s
+      retries: 10
+      start_period: 60s
   nlu:
     image: ambiverse/ambiverse-nlu
     restart: always
-    depends_on:
-      - db
     ports:
       - 8080:8080
     environment:
       AIDA_CONF: aida_20180120_cs_de_en_es_ru_zh_v18_cass
+      DATABASE: aida_20180120_cs_de_en_es_ru_zh_v18
+      DOWNLOAD_HOST: http://ambiversenlu-download.mpi-inf.mpg.de/
+    entrypoint: [ "/cassandra-init.sh", "http://ambiversenlu-download.mpi-inf.mpg.de/", "-d", "aida_20180120_cs_de_en_es_ru_zh_v18", "-ch", "nlu-cassandra", "-cp","9042", "-s", "-t", "720", "--", "mvn", "jetty:run" ]
+    deploy:
+      replicas: 1
+        resources:
+          limits:
+            memory: 44G
+    volumes:
+      - "nlu-caches:/var/lib/jetty/caches/aida_20180120_cs_de_en_es_ru_zh_v18_cass/"
+      - "nlu-logs:/var/lib/jetty/logs"
+      - type: tmpfs
+        target: /var/tmp/data
+        tmpfs:
+        size: 107374182400
+    networks:
+      - nlunet
+    healthcheck:
+      test: curl -sS http://127.0.0.1:8080/v2/entitylinking/analyze/_status || exit 1
+      interval: 60s
+      timeout: 60s
+      retries: 10
+      start_period: 5m
+
+volumes:
+  cassandra_data:
+  nlu-caches:
+  nlu-logs:
+
+networks:
+  nlunet:
 ~~~~~~~~
 
 Run `docker stack deploy -c service-cassandra.yml cassandra` (or `docker-compose -f service-cassandra.yml up`), wait for it to initialize completely.
-
-If you want to create a cluster of nodes, you can add another db service, and add the `CASSANDRA_SEEDS` env variable with values of both services, like this:
-
-~~~~~~~~
-version: '3.1'
-
-services:
-
-  db:
-    image: ambiverse/nlu-db-cassandra
-    restart: always
-    environment:
-      DATABASE_NAME: aida_20180120_2f_de_en_v18
-      CASSANDRA_SEEDS: db,db1
-  db1:
-      image: ambiverse/nlu-db-cassandra
-      restart: always
-      environment:
-        DATABASE_NAME: aida_20180120_2f_de_en_v18
-        CASSANDRA_SEEDS: db,db1
-
-  nlu:
-    image: ambiverse/ambiverse-nlu
-    restart: always
-    depends_on:
-      - db
-      - db1
-    ports:
-      - 8080:8080
-    environment:
-      AIDA_CONF: aida_20180120_2f_de_en_v18_cass
-~~~~~~~~
--- nlu-db-cassandra/docker-entrypoint.sh
+++ nlu-db-cassandra/docker-entrypoint.sh
@@ -1,90 +1,6 @@
 #!/bin/bash
 set -e
 
-# execute a cql file as a string statement to cqlsh
-_execute() {
-	statement=$(<$1)
-	until echo "$statement" | cqlsh; do
-		echo "cqlsh: Cassandra is unavailable - retry later"
-		sleep 2
-	done
-}
-
-# determing how to execute the file based on extention
-_process_init_file() {
-	local f="$1"; shift
-
-	case "$f" in
- 		*.sh)     echo "$0: running $f"; . "$f" ;;
-		*.cql)    echo "$0: running $f"; _execute "$f"; echo ;;
-		*.cql.gz) echo "$0: running $f"; gunzip -c "$f" | _execute; echo ;;
-		*)        echo "$0: ignoring $f" ;;
-	esac
-}
-
-_check_files() {
-	until cqlsh -e 'describe cluster'; do
-		# processing the files in the data directory
-		echo "Cassandra service is still starting up, waiting for it...";
-		sleep 30
-	done
-	echo "Cassandra service ready, starting to import.";
-	for f in ${DATA_PATH}/*; do
-		echo "processing file $f"
-		_process_init_file "$f"
-	done
-}
-
-
-_extract_data() {
-    if [ "$DATABASE_NAME" ]
-    then
-        if [ -d "${DATA_PATH}" ]
-        then
-            cd ${DATA_PATH}/
-            #Move the data to the folders
-            for D in `find . -maxdepth 1 -type d -printf "%f\n"`
-            do
-              echo "Moving files from: $D"
-              cd ${DATA_PATH}/$D
-              for D1 in `find . -maxdepth 1 -type d -printf "%f\n"`
-              do
-                if [ -d $D1/snapshots/*/ ]
-                then
-                  mv $D1/snapshots/*/* /var/lib/cassandra/data/${D}/$(echo $D1| cut -d'-' -f 1)*/
-                   #refresh the moved data
-                  echo "Refreshing data for ${D} $(echo $D1| cut -d'-' -f 1)"
-                  nodetool refresh ${D} $(echo $D1| cut -d'-' -f 1)
-                fi
-              done
-              cd ..
-            done
-         fi
-     fi
-}
-
-_remove_data_tmp(){
-    if [ "${DATA_PATH}" ]
-    then
-        cd ${DATA_PATH}/
-        rm -rf *
-    fi
-}
-
-_download_dump(){
-    local dump="$1"
-    if [ ! -d "/var/lib/cassandra/data/$dump" ]
-    then
-       echo "Downloading dump from http://ambiversenlu-download.mpi-inf.mpg.de/cassandra/$dump.tar.gz ..."
-
-       wget -q --no-cookies -O - "http://ambiversenlu-download.mpi-inf.mpg.de/cassandra/$dump.tar.gz" \
-       | tar xz --directory=${DATA_PATH} -f -
-       echo "Download finished!"
-    else
-        echo "Using existing keyspace $dump ."
-    fi
-}
-
 # first arg is `-f` or `--some-option`
 # or there are no args
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
@@ -93,49 +9,33 @@ fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'cassandra' -a "$(id -u)" = '0' ]; then
-	chown -R cassandra /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG"
+	find /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG" \
+		\! -user cassandra -exec chown cassandra '{}' +
 	exec gosu cassandra "$BASH_SOURCE" "$@"
 fi
 
 _ip_address() {
 	# scrape the first non-localhost IP address of the container
 	# in Swarm Mode, we often get two IPs -- the container IP, and the (shared) VIP, and the container IP should always be first
-	ip address | awk '
-		$1 == "inet" && $NF != "lo" {
-			gsub(/\/.+$/, "", $2)
-			print $2
-			exit
-		}
-	'
+	#ip address | awk '
+	#	$1 == "inet" && $NF != "lo" {
+	#		gsub(/\/.+$/, "", $2)
+	#		print $2
+	#		exit
+	#	}
+	#'
+	ip -o -4 addr list eth0 | sed -n 1p | awk '{print $4}' | cut -d/ -f1
 }
 
-
-# first test if the "data" directory exists and contains files
-if [ -d "${DATA_PATH}" ]; then
-
-    if [ "$DATABASE_NAME" ]; then
-        _remove_data_tmp
-         _download_dump "${DATABASE_NAME}"
-    fi
-
-	if [ ! -z "$(ls -A ${DATA_PATH})" ]; then
-		# if there is an init-db.cql file, we probably want it to be executed first
-		# I rename it because scripts are executed in alphabetic order
-		exec "$@" &
-		if [ -e ${DATA_PATH}/init-db.cql ]
-		then
-			echo "Found an init-db.cql file"
-			mv ${DATA_PATH}/init-db.cql ${DATA_PATH}/1-init-db.cql
-		fi
-		_check_files
-	    #Extract the data after the keyspace schema is created
-
-	    _extract_data
-		# after the files are loaded, restart cassandra with the normal settings.
-		pkill -f 'java.*cassandra'
-	fi
-fi
-
+# "sed -i", but without "mv" (which doesn't work on a bind-mounted file, for example)
+_sed-in-place() {
+	local filename="$1"; shift
+	local tempFile
+	tempFile="$(mktemp)"
+	sed "$@" "$filename" > "$tempFile"
+	cat "$tempFile" > "$filename"
+	rm "$tempFile"
+}
 
 if [ "$1" = 'cassandra' ]; then
 	: ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
@@ -157,7 +57,8 @@ if [ "$1" = 'cassandra' ]; then
 	fi
 	: ${CASSANDRA_SEEDS:="$CASSANDRA_BROADCAST_ADDRESS"}
 
-	sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
+	_sed-in-place "$CASSANDRA_CONFIG/cassandra.yaml" \
+		-r 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/'
 
 	for yaml in \
 		broadcast_address \
@@ -172,7 +73,8 @@ if [ "$1" = 'cassandra' ]; then
 		var="CASSANDRA_${yaml^^}"
 		val="${!var}"
 		if [ "$val" ]; then
-			sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
+			_sed-in-place "$CASSANDRA_CONFIG/cassandra.yaml" \
+				-r 's/^(# )?('"$yaml"':).*/\2 '"$val"'/'
 		fi
 	done
 
@@ -180,7 +82,8 @@ if [ "$1" = 'cassandra' ]; then
 		var="CASSANDRA_${rackdc^^}"
 		val="${!var}"
 		if [ "$val" ]; then
-			sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
+			_sed-in-place "$CASSANDRA_CONFIG/cassandra-rackdc.properties" \
+				-r 's/^('"$rackdc"'=).*/\1 '"$val"'/'
 		fi
 	done
 fi
